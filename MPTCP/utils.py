056798179ef566f66864182e48bd557a22f01711
Sreekar Venkata Nutulapati <sreekarnv1@gmail.com>
2021-04-24 16:19:09 +0530

    updated utils.py

--- MPTCP/utils.py
+++ MPTCP/utils.py
@@ -1,7 +1,13 @@
 import numpy as np
 
 def encodeNumPyArray(fr):
-    nor = len(fr).to_bytes(2,'big') # number of rows
-    noc = len(fr[0]).to_bytes(2, 'big') # number of cols
-    temp = np.array([nor[0],nor[1],noc[0],noc[1]], np.uint8)
+    # nor = len(fr).to_bytes(2,'big') # number of rows
+    # noc = len(fr[0]).to_bytes(2, 'big') # number of cols
+    # temp = np.array([nor[0],nor[1],noc[0],noc[1]], np.uint8)
+    # return np.concatenate((temp, fr), axis=None).tobytes()
+    
+    nor = len(fr / 2).to_bytes(2, 'big')
+    noc = len(fr / 2).to_bytes(2, 'big')
+    temp = np.array([nor, noc], np.uint8) 
     return np.concatenate((temp, fr), axis=None).tobytes()
+    
